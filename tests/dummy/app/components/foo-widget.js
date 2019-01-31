9ba9da0c35698a1ee3d940db1de2c3d03a1efef6
David J. Hamilton <dhamilton@linkedin.com>
2019-01-30 19:08:23 -0800

    Simplify dummy app

--- tests/dummy/app/components/foo-widget.js
+++ tests/dummy/app/components/foo-widget.js
@@ -1,45 +1,50 @@
 import Component from '@ember/component';
 import layout from '../templates/components/foo-widget';
-import { observer } from '@ember/object';
-import { A } from '@ember/array';
-import ArrayProxy from '@ember/array/proxy';
+import { computed } from '@ember/object';
+import { alias } from '@ember/object/computed';
+import { alias as aliasCp } from 'ember-alias-cps';
+
+let i=0;
 
 export default Component.extend({
   layout,
 
-  // alias
-  chapterCountΔ: 0,
-  bitsCountΔ: 0,
-
-  // alias CP
-  acpChapterCountΔ: 0,
-  acpBitsCountΔ: 0,
-
-  incrementChapterCountΔ: observer('book.chapterCount', function incrementChapterCountΔ() {
-    this.incrementProperty('chapterCountΔ');
+  authorName: computed('book.author', function () {
+    return this.get('book.author');
   }),
 
-  incrementbitsCountΔ: observer('book.bitsCount', function incrementbitsCountΔ() {
-    this.incrementProperty('bitsCountΔ');
+  writerName: computed('altBook.author', function () {
+    return this.get('altBook.author');
   }),
 
-  incrementAcpacpChapterCountΔ: observer('book.acpChapterCount', function incrementAcpacpChapterCountΔ() {
-    this.incrementProperty('acpChapterCountΔ');
+  book: alias('bookThing'),
+  altBook: aliasCp('altBookThing'),
+
+
+  bookThing: computed('bookData', function () {
+    // during pingBook this is *eagerly* fetched during the property change
+    // event for `book`, due to eager chain revalidation as alias is volatile
+    return this.get('bookData');
+  }),
+  altBookThing: computed('altBookData', function () {
+    // during pingAltBook() this is retrieved during re-rendering, as expected
+    return this.get('altBookData');
   }),
 
-  incrementAcpBitsCountΔ: observer('book.acpBitsCount', function incrementAcpBitsCountΔ() {
-    this.incrementProperty('acpBitsCountΔ');
-  }),
+  bookData: null,
+  altBookData: null,
 
   actions: {
-    addBits() {
-      this.book.get('chapters').pushObjects(['three', 'four', 'five']);
+    pingBook() {
+      this.set('bookData', {
+        author: `Das Author ${++i}`,
+      });
     },
 
-    reset() {
-      this.set('book.bits', new ArrayProxy({
-        content: A(['one', 'two', 'three'])
-      }));
+    pingAltBook() {
+      this.set('altBookData', {
+        author: `Das AltBook Author ${++i}`,
+      });
     }
   }
 });
